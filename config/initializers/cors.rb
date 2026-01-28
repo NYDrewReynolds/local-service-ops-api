6b399b9dea30d113c1d98868f6d8f733cf1dc4ff
Drew Reynolds <drew@juniphq.com>
2026-01-28 11:47:28 -0500

    Expand CORS origins for dev ports
    Allow multiple dashboard origins so preflight requests from 8080 pass.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("DASHBOARD_ORIGIN", "http://localhost:3000")
+    default_origins = ["http://localhost:8080", "http://localhost:3000"]
+    env_origins = ENV.fetch("DASHBOARD_ORIGINS", "").split(",").map(&:strip).reject(&:empty?)
+
+    origins(*(default_origins + env_origins))
     resource "/api/*",
              headers: :any,
              methods: %i[get post patch put delete options head],
